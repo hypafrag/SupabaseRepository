03bbb9472d9728e65c14c5dc195614d167c63e1b
Ilya Kuznetsov <i.v.kuznecov@gmail.com>
2024-07-02 18:46:28 +0200

    fixed realtime

--- Sources/SupabaseRepository/Utils/DidChangePublisher.swift
+++ Sources/SupabaseRepository/Utils/DidChangePublisher.swift
@@ -30,13 +30,13 @@ public struct DidChangePublisher: Publisher {
     public typealias Output = RemoteChange
     public typealias Failure = Never
     
-    private let channel: RealtimeChannelV2
+    private let client: SupabaseClient
     private let table: SupabaseId
-    private let filter: String
+    private let filter: String?
     private let changes: Changes
     
-    public init(client: SupabaseClient, table: SupabaseId, filter: String, changes: Changes = [.added, .modified, .removed]) {
-        self.channel = client.realtimeV2.channel(table.rawValue)
+    public init(client: SupabaseClient, table: SupabaseId, filter: String?, changes: Changes = [.added, .modified, .removed]) {
+        self.client = client
         self.table = table
         self.filter = filter
         self.changes = changes
@@ -44,29 +44,35 @@ public struct DidChangePublisher: Publisher {
     
     private final class PublisherSubscription<S: Subscriber>: Subscription where S.Input == Output {
         
+        let client: SupabaseClient
         let channel: RealtimeChannelV2
         @RWAtomic var tokens: [RealtimeChannelV2.Subscription] = []
         
-        init(subscriber: S, channel: RealtimeChannelV2, table: SupabaseId, filter: String, changes: Changes)  {
-            self.channel = channel
-            if changes.contains(.added) {
-                tokens.append(channel.onPostgresChange(InsertAction.self, table: table.rawValue, filter: filter) { insert in
-                    _ = subscriber.receive(.added(insert.record.anyDictionary))
-                })
-            }
+        init(subscriber: S, client: SupabaseClient, table: SupabaseId, filter: String?, changes: Changes) {
+            self.client = client
+            self.channel = client.realtimeV2.channel(table.rawValue)
+            
+            Task {
+                if changes.contains(.added) {
+                    tokens.append(channel.onPostgresChange(InsertAction.self, table: table.rawValue, filter: filter) { insert in
+                        _ = subscriber.receive(.added(insert.record.anyDictionary))
+                    })
+                }
+                    
+                if changes.contains(.removed) {
+                    tokens.append(channel.onPostgresChange(DeleteAction.self, table: table.rawValue, filter: filter) { delete in
+                        _ = subscriber.receive(.removed(delete.oldRecord.anyDictionary))
+                    })
+                }
+                    
+                if changes.contains(.modified) {
+                    tokens.append(channel.onPostgresChange(UpdateAction.self, table: table.rawValue, filter: filter) { update in
+                        _ = subscriber.receive(.modified(update.record.anyDictionary))
+                    })
+                }
                 
-            if changes.contains(.removed) {
-                tokens.append(channel.onPostgresChange(DeleteAction.self, table: table.rawValue, filter: filter) { delete in
-                    _ = subscriber.receive(.removed(delete.oldRecord.anyDictionary))
-                })
+                await channel.subscribe()
             }
-                
-            if changes.contains(.modified) {
-                tokens.append(channel.onPostgresChange(UpdateAction.self, table: table.rawValue, filter: filter) { update in
-                    _ = subscriber.receive(.modified(update.record.anyDictionary))
-                })
-            }
-            Task { await channel.subscribe() }
         }
         
         func request(_ demand: Subscribers.Demand) { }
@@ -74,15 +80,15 @@ public struct DidChangePublisher: Publisher {
         func cancel() {
             tokens.forEach { $0.cancel() }
             tokens.removeAll()
-        }
-        
-        deinit { 
-            tokens.forEach { $0.cancel() }
-            tokens.removeAll()
+            
+            Task { [channel, client] in
+                await channel.unsubscribe()
+                await client.removeChannel(channel)
+            }
         }
     }
 
     public func receive<S: Subscriber>(subscriber: S) where S.Input == Output, S.Failure == Failure {
-        subscriber.receive(subscription: PublisherSubscription<S>(subscriber: subscriber, channel: channel, table: table, filter: filter, changes: changes))
+        subscriber.receive(subscription: PublisherSubscription<S>(subscriber: subscriber, client: client, table: table, filter: filter, changes: changes))
     }
 }
